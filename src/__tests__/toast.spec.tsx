02704fe7425a03a278d0acab04827593b94666ba
Steven-failla_mmcgh <steven.failla@nera.com>
2023-02-05 09:17:30 -0500

    update test files

--- src/__tests__/toast.spec.tsx
+++ src/__tests__/toast.spec.tsx
@@ -1,46 +1,44 @@
-import { render, userEvent } from '../test/test-utils'
+import { render, userEvent, screen } from '../test/test-utils'
 import ToastNotification from '../components/toast'
 import { mockArticle, mockUser } from '../test/data/mockData'
 
 // mock out articleContext
 jest.mock('../context/articleContext', () => ({
-  __esModule: true,
-  useArticleContext: jest.fn(() => mockArticle),
-  ArticleProvider: ({ children }: { children: React.ReactNode }) => children
+	__esModule: true,
+	useArticleContext: jest.fn(() => mockArticle),
+	ArticleProvider: ({ children }: { children: React.ReactNode }) => children
 }))
 
 // mock authContext
 jest.mock('../context/authContext', () => ({
-  __esModule: true,
-  useAuthContext: jest.fn(() => mockUser),
-  AuthProvider: ({ children }: { children: React.ReactNode }) => children
+	__esModule: true,
+	useAuthContext: jest.fn(() => mockUser),
+	AuthProvider: ({ children }: { children: React.ReactNode }) => children
 }))
 
 describe('toast notification unit tests', () => {
-  const message: string = 'test message'
+	const message: string = 'test message'
 
-  test('Toast should display and close correctly', async () => {
-    const { getByText, getByRole, queryByText } = await render(
-      <ToastNotification variant="success" message={message} />
-    )
-    expect(getByText(message)).toBeInTheDocument()
-    userEvent.click(getByRole('button'))
-    expect(queryByText(message)).not.toBeInTheDocument()
-  })
+	test('Toast should display and close correctly', async () => {
+		await render(<ToastNotification variant='success' message={message} />)
+		expect(screen.getByText(message)).toBeInTheDocument()
+		userEvent.click(screen.getByRole('button'))
+		expect(screen.queryByText(message)).not.toBeInTheDocument()
+	})
 
-  test('should change color depending on variant', async () => {
-    const { rerender, container } = await render(
-      <ToastNotification variant="success" message={message} />
-    )
-    expect(container.firstChild).toHaveStyle('border-color: #47d764')
+	test('should change color depending on variant', async () => {
+		const { rerender, container } = await render(
+			<ToastNotification variant='success' message={message} />
+		)
+		expect(container.firstChild).toHaveStyle('border-color: #47d764')
 
-    rerender(<ToastNotification variant="error" message={message} />)
-    expect(container.firstChild).toHaveStyle('border-color: #dc3545')
+		rerender(<ToastNotification variant='error' message={message} />)
+		expect(container.firstChild).toHaveStyle('border-color: #dc3545')
 
-    rerender(<ToastNotification variant="info" message={message} />)
-    expect(container.firstChild).toHaveStyle('border-color: #2f86eb')
+		rerender(<ToastNotification variant='info' message={message} />)
+		expect(container.firstChild).toHaveStyle('border-color: #2f86eb')
 
-    rerender(<ToastNotification variant="warning" message={message} />)
-    expect(container.firstChild).toHaveStyle('border-color: #ffc021')
-  })
+		rerender(<ToastNotification variant='warning' message={message} />)
+		expect(container.firstChild).toHaveStyle('border-color: #ffc021')
+	})
 })
--- src/__tests__/toggle.spec.tsx
+++ src/__tests__/toggle.spec.tsx
@@ -1,42 +1,42 @@
 // TOGGLE COMPONENT IMPORTS
-import { render, userEvent } from '../test/test-utils'
+import { render, userEvent, screen } from '../test/test-utils'
 import Toggle from '../components/toggle/index'
 import { mockArticle, mockUser } from '../test/data/mockData'
 
 // mock out articleContext
 jest.mock('../context/articleContext', () => ({
-  __esModule: true,
-  useArticleContext: jest.fn(() => mockArticle),
-  ArticleProvider: ({ children }: { children: React.ReactNode }) => children
+	__esModule: true,
+	useArticleContext: jest.fn(() => mockArticle),
+	ArticleProvider: ({ children }: { children: React.ReactNode }) => children
 }))
 
 // mock authContext
 jest.mock('../context/authContext', () => ({
-  __esModule: true,
-  useAuthContext: jest.fn(() => mockUser),
-  AuthProvider: ({ children }: { children: React.ReactNode }) => children
+	__esModule: true,
+	useAuthContext: jest.fn(() => mockUser),
+	AuthProvider: ({ children }: { children: React.ReactNode }) => children
 }))
 
 describe('Toggle', () => {
-  test('toggle triggers light/dark mode colors', async () => {
-    const { getByRole, getByLabelText } = await render(<Toggle />)
+	test('toggle triggers light/dark mode colors', async () => {
+		await render(<Toggle />)
 
-    const toggleButton = getByRole('button')
-    const sunIcon = getByLabelText('sun-icon-label').querySelector('svg')!
-    const moonIcon = getByLabelText('moon-icon-label').querySelector('svg')!
-    const toggle = () => userEvent.click(toggleButton)
+		const toggleButton = screen.getByRole('button')
+		const sunIcon = screen.getByLabelText('sun-icon-label').querySelector('svg')!
+		const moonIcon = screen.getByLabelText('moon-icon-label').querySelector('svg')!
+		const toggle = () => userEvent.click(toggleButton)
 
-    expect(sunIcon.getAttribute('fill')).toBe('gold')
-    expect(moonIcon.getAttribute('fill')).toBe('#b8b8b8')
+		expect(sunIcon.getAttribute('fill')).toBe('gold')
+		expect(moonIcon.getAttribute('fill')).toBe('#b8b8b8')
 
-    toggle()
+		toggle()
 
-    expect(sunIcon.getAttribute('fill')).toBe('white')
-    expect(moonIcon.getAttribute('fill')).toBe('gold')
+		expect(sunIcon.getAttribute('fill')).toBe('white')
+		expect(moonIcon.getAttribute('fill')).toBe('gold')
 
-    toggle()
+		toggle()
 
-    expect(sunIcon.getAttribute('fill')).toBe('gold')
-    expect(moonIcon.getAttribute('fill')).toBe('#b8b8b8')
-  })
+		expect(sunIcon.getAttribute('fill')).toBe('gold')
+		expect(moonIcon.getAttribute('fill')).toBe('#b8b8b8')
+	})
 })
