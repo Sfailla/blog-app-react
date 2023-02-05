499b651f5b5425ef38b49053ac2dbe1615c7ef63
Steven-failla_mmcgh <steven.failla@nera.com>
2023-02-05 09:16:29 -0500

    change color palette for light mode in theme

--- src/components/banner/style.ts
+++ src/components/banner/style.ts
@@ -3,35 +3,35 @@ import { flex } from '../../styles/mixins'
 import { DesignSystem } from '../../styles/shared'
 
 const {
-  color,
-  layout: { layoutWrapper },
-  typography: { heading_lg, gradientText }
+	color,
+	layout: { layoutWrapper },
+	typography: { heading_lg, gradientText }
 } = DesignSystem
 
 export const Container = styled.div`
-  width: 100%;
-  height: 17rem;
-  padding: 0 2rem;
-  background-color: ${({ theme }) => theme.background.tertiary};
-  box-shadow: inset 0 8px 8px -8px rgb(0 0 0 / 30%), inset 0 -8px 8px -8px rgb(0 0 0 / 30%);
-  ${flex()};
+	width: 100%;
+	height: 17rem;
+	padding: 0 2rem;
+	background-color: ${({ theme }) => theme.background.secondary};
+	box-shadow: inset 0 8px 8px -8px rgb(0 0 0 / 30%), inset 0 -8px 8px -8px rgb(0 0 0 / 30%);
+	${flex()};
 `
 
 export const TextWrapper = styled.div`
-  ${layoutWrapper()};
-  text-align: left;
+	${layoutWrapper()};
+	text-align: left;
 `
 
 export const Title = styled.h1`
-  font-size: 5rem;
-  font-family: monospace;
-  ${heading_lg()};
-  ${gradientText()};
+	font-size: 5rem;
+	font-family: monospace;
+	${heading_lg()};
+	${gradientText()};
 `
 
 export const Subtitle = styled.h3`
-  font-weight: normal;
-  margin-top: 1rem;
-  letter-spacing: 1px;
-  color: ${color.secondary.white};
+	font-weight: normal;
+	margin-top: 1rem;
+	letter-spacing: 1px;
+	color: ${color.secondary.white};
 `
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,69 +1,66 @@
-export type ThemeType = typeof theme
+// export type ThemeType = typeof theme
 
 export const theme = {
-  light: {
-    background: {
-      primary: '#FFFFFF',
-      secondary: '#105151',
-      tertiary: '#105151',
-      text: '#072f3a'
-    },
-    text: {
-      primary: '#19202D',
-      primaryLight: '#19202D',
-      secondary: '#105151',
-      secondaryLight: '#105151',
-      heading: '#1f3b6f',
-      gray: 'darkgray',
-      comment: 'darkgray'
-    },
-    tag: {
-      small: '#0085FF'
-    },
-    link: {
-      main: '#0000EE',
-      hover: '#8957e5'
-    },
-    button: {
-      normal: '#105151',
-      preview: '#000000'
-    },
-    border: {
-      light: 'darkgray'
-    }
-  },
-  dark: {
-    background: {
-      primary: '#060C20',
-      secondary: '#112240',
-      tertiary: '#122d5e',
-      text: '#FFFFFF'
-    },
-    text: {
-      primary: '#FFFFFF',
-      primaryLight: '#d8d7d7',
-      secondary: '#61B3FB',
-      secondaryLight: '#64ffda',
-      heading: '#FFFFFF',
-      gray: '#19202D',
-      comment: '#787878'
-    },
-    tag: {
-      small: '#0085FF'
-    },
-    link: {
-      main: '#8957e5',
-      hover: '#0000EE'
-    },
-    button: {
-      normal: '#0f8fff',
-      preview: '#e9e9e9'
-    },
-    border: {
-      light: '#464646'
-    }
-  }
-}
-
-// secondary: '#1D2335'
-// secondary: '#64ffda'
+	light: {
+		background: {
+			primary: '#FFFFFF',
+			secondary: '#112240',
+			tertiary: '#122d5e',
+			text: '#072f3a'
+		},
+		text: {
+			primary: '#19202D',
+			primaryLight: '#19202D',
+			secondary: '#122d5e',
+			secondaryLight: '#122d5e',
+			heading: '#1f3b6f',
+			gray: 'darkgray',
+			comment: 'darkgray'
+		},
+		tag: {
+			small: '#0085FF'
+		},
+		link: {
+			main: '#0000EE',
+			hover: '#8957e5'
+		},
+		button: {
+			normal: '#122d5e',
+			preview: '#000000'
+		},
+		border: {
+			light: 'darkgray'
+		}
+	},
+	dark: {
+		background: {
+			primary: '#060C20',
+			secondary: '#112240',
+			tertiary: '#122d5e',
+			text: '#FFFFFF'
+		},
+		text: {
+			primary: '#FFFFFF',
+			primaryLight: '#d8d7d7',
+			secondary: '#61B3FB',
+			secondaryLight: '#64ffda',
+			heading: '#FFFFFF',
+			gray: '#19202D',
+			comment: '#787878'
+		},
+		tag: {
+			small: '#0085FF'
+		},
+		link: {
+			main: '#8957e5',
+			hover: '#0000EE'
+		},
+		button: {
+			normal: '#0f8fff',
+			preview: '#e9e9e9'
+		},
+		border: {
+			light: '#464646'
+		}
+	}
+} as const
